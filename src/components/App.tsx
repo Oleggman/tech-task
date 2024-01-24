5bb725e3b89220a1e27789bc6c84fb212f30a53d
Олег <olegkovalets1112003@knu.ua>
2024-01-24 22:54:08 +0200

    Add react router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import { lazy } from "react";
+import { SharedLayout } from "./SharedLayout/SharedLayout";
+
+const HomePage: React.LazyExoticComponent<() => JSX.Element> = lazy(() => import('../pages/Home'));
+const BoardPage: React.LazyExoticComponent<() => JSX.Element> = lazy(() => import('../pages/Board'));
+
 const App = () => {
   return (
-    <div className="App">
-      App component
-    </div>
+    <Routes>
+      <Route path='/' element={<SharedLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="boards/:boardId" element={<BoardPage />} />
+        <Route path="*" element={<HomePage />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyle } from './components/GlobalStyle';
 
-const root = ReactDOM.createRoot(
+const root: ReactDOM.Root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <BrowserRouter basename="/tech-task"> */}
+    <BrowserRouter>
+      <App />
+      <GlobalStyle />
+    </BrowserRouter>
   </React.StrictMode>
 );
