a5b34397ed9194c88f51a7b0727f75307c868259
Олег <olegkovalets1112003@knu.ua>
2024-01-25 17:38:45 +0200

    Updates

---
+++ static/js/293.65d22593.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktech_task=self.webpackChunktech_task||[]).push([[293],{293:(e,s,t)=>{t.r(s),t.d(s,{default:()=>c});var a=t(184);const c=()=>(0,a.jsx)("h1",{children:"Home Page"})}}]);
+//# sourceMappingURL=293.65d22593.chunk.js.map
